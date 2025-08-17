82e87e558d1dd0d6289c64c129c39500fa637e89
hadi14250 <hadihk2k@gmail.com>
2025-08-18 03:46:43 +0400

    done

--- app/Contact/page.tsx
+++ app/Contact/page.tsx
@@ -102,7 +102,7 @@ export default function Contact() {
                     </svg>
                   </div>
                   <div>
-                    <h3 className="text-lg font-semibold text-gray-900">Phone</h3>
+                    <h3 className="text-lg font-semibold text-gray-900">UAE</h3>
                     <div className="space-y-4">
                       <div>
                         <p className="text-gray-600">Eng. Mahfouz Alhalouch:</p>
@@ -125,7 +125,29 @@ export default function Contact() {
                           +971 55 547 1511
                         </a>
                       </div>
+
                     </div>
+
+
+
+                  </div>
+
+                  <div>
+                    <h3 className="text-lg font-semibold text-gray-900">UK</h3>
+                    <div className="space-y-4">
+
+
+                      <div>
+                        <p className="text-gray-600">Amal Aljord:</p>
+                        <a
+                          href="tel:+44 7464 239684"
+                          className="text-gray-600 hover:text-gray-800 transition-colors duration-300"
+                        >
+                          +44 7464 239684
+                        </a>
+                      </div>
+                    </div>
+
 
 
                   </div>
@@ -139,9 +161,24 @@ export default function Contact() {
                     </svg>
                   </div>
                   <div>
-                    <h3 className="text-lg font-semibold text-gray-900">Address</h3>
+                    <h3 className="text-lg font-semibold text-gray-900">UAE Address</h3>
                     <p className="text-gray-600">Deira, Al Rigga, Al Maktoum rd</p>
                     <p className="text-gray-600">Damas Dubai Tower</p>
+                  </div>
+                </div>
+
+
+                <div className="flex items-start space-x-4">
+                  <div className="flex-shrink-0">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-blue-900" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
+                    </svg>
+                  </div>
+                  <div>
+                    <h3 className="text-lg font-semibold text-gray-900">UK Address</h3>
+                    <p className="text-gray-600">London, 5 Brayford Square</p>
+                    <p className="text-gray-600">E1 0SG</p>
                   </div>
                 </div>
 
